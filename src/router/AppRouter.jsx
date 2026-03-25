691a83eef12048b35d0c72cae1e0bad5627a19c0
NguyenHai <156715672+Nonedaw@users.noreply.github.com>
2026-03-25 14:43:39 +0700

    a

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -53,6 +53,7 @@ const AppRouter = () => {
         <Route path="rooms" element={<AdminRooms />} />
       </Route>
 
+<<<<<<< Updated upstream
       {/* Staff */}
       <Route
         path="/staff"
@@ -70,6 +71,61 @@ const AppRouter = () => {
       <Route path="*" element={<Navigate to={getRedirectPath()} />} />
     </Routes>
   );
+=======
+                {/* Thông tin chính sách */}
+                <Route path="/info" element={<PolicyLayout/>}>
+                    <Route path="about" element={<AboutUs/>}/>
+                    <Route path="contact" element={<Contact/>}/>
+                    <Route path="terms" element={<TermsConditions/>}/>
+                    <Route path="privacy" element={<PrivacyPolicy/>}/>
+                    <Route path="refund" element={<RefundPolicy/>}/>
+                    <Route path="faq" element={<Faq/>}/>
+                </Route>
+            </Route>
+
+            {/* 2. Nhóm Route Auth */}
+            <Route path="/oauth2/redirect" element={<OAuth2RedirectHandler/>}/>
+            <Route
+                path="/login"
+                element={!auth ? <Login/> : <Navigate to={getRedirectPath()}/>}
+            />
+            <Route
+                path="/register"
+                element={!auth ? <Register/> : <Navigate to={getRedirectPath()}/>}
+            />
+            <Route path="/forgot-password" element={<ForgotPassword/>}/>
+
+            {/* 3. Nhóm Route Admin */}
+            <Route
+                path="/admin"
+                element={<ProtectedRoute allowedRoles={[ROLE_ADMIN]}><AdminLayout/></ProtectedRoute>}
+            >
+                <Route index element={<AdminDashboard/>}/>
+                <Route path="movies" element={<AdminMovies/>}/>
+                <Route path="rooms" element={<AdminRooms/>}/>
+                <Route path="users" element={<AdminUsers/>}/>
+                <Route path="staffs" element={<AdminStaffs/>}/>
+                <Route path="vouchers" element={<AdminVouchers/>}/>
+                <Route path="showtimes" element={<AdminShowtimes/>}/>
+                <Route path="bookings" element={<AdminBookings/>}/>
+                <Route path="profile" element={<ProfilePage/>}/>
+            </Route>
+
+            {/* 4. Staff */}
+
+            <Route path="/staff"
+                   element={<ProtectedRoute allowedRoles={['STAFF', 'ADMIN']}><StaffLayout/></ProtectedRoute>}>
+                <Route index element={<Navigate to="/staff/dashboard" replace/>}/>
+                <Route path="dashboard" element={<StaffDashboard/>}/>
+                <Route path="bookings" element={<StaffBookings/>}/>
+                {<Route path="pos" element={<StaffPOS />} />}
+            </Route>
+
+            {/* 5. Điều hướng mặc định */}
+            <Route path="*" element={<Navigate to={getRedirectPath()}/>}/>
+        </Routes>
+    );
+>>>>>>> Stashed changes
 };
 
 export default AppRouter;
